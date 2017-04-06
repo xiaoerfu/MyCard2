de327089abfdbb04355cc3695b0b4d2caec7145e
lijianfu <3067418132@qq.com>
2017-04-06 17:46:02 +0800

    导航栏可以初步显示，但未能在指定的地方显示内容

---
+++ app/src/main/java/com/example/mycard/Fragment/TabDb.java
@@ -0,0 +1,26 @@
+package com.example.mycard.Fragment;
+
+import com.example.mycard.R;
+
+/**
+ * Created by lijianfu on 2017/4/6.
+ */
+public class TabDb {
+
+    public static String[] getTabsTxt(){
+        String[] tabs={"初始化","信息中心","充值中心"};
+        return tabs;
+    }
+    public static int[] getTabsImg(){
+        int[] ids={R.drawable.news,R.drawable.news, R.drawable.news};
+        return ids;
+    }
+    public static int[] getTabsImgLight(){
+        int[] ids={R.drawable.news,R.drawable.news,R.drawable.news};
+        return ids;
+    }
+    public static Class[] getFragments(){
+        Class[] clz={InitFragment.class,MessageFragment.class,RechargeFragment.class};
+        return clz;
+    }
+}
--- app/src/main/java/com/example/mycard/MainActivity.java
+++ app/src/main/java/com/example/mycard/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.mycard;
 
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -9,42 +11,34 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TabHost;
+import android.widget.TabWidget;
 import android.widget.TextView;
 
-import com.example.mycard.Fragment.InitFragment;
-import com.example.mycard.Fragment.MessageFragment;
-import com.example.mycard.Fragment.RechargeFragment;
-import static android.R.id.tabcontent;
+import com.example.mycard.Fragment.TabDb;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TabHost.OnTabChangeListener {
 
     private DrawerLayout mDrawerLayout;                 //侧滑菜单
     private NavigationView navView;                           //侧滑菜单项目
+    private FragmentTabHost tabHost;                        //导航栏
 
-    //添加Fragment的数组,主要是为了动态的改变App,一级界面的个数,如果要添加新的模块,只需要在这里添加一个Fragment
-    private Class[] fragments = new Class[]{InitFragment.class, MessageFragment.class, RechargeFragment.class};
-    //为框架底部设置图片的状态选择器数组,如果需要添加新的模块,就需要添加状态选择器
-    private int[] imgSelectors = new int[]{R.drawable.ic_menu, R.drawable.ic_backup, R.drawable.ic_comment};
-    //为框架底部设置文本资源,如果要添加新的模块,还要在这里添加文本信息
-    private String[] tabTitle = new String[]{"初始化", "信息中心", "充值中心"};
-
-
-    ImageView tab_iv;
-    TextView tab_tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         /*加载菜单主页图标*/
         mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null){
+        if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
         }
@@ -61,24 +55,53 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //对控件初始化
-        FragmentTabHost tabHost = (FragmentTabHost) findViewById(R.id.tabHost);
-        //为FragmentTabHost来填充Fragment,1:上下文  2:fragment的管理器FragmentManager 3:官方要求写死的参数
-        tabHost.setup(MainActivity.this, getSupportFragmentManager(),tabcontent);
+        /*初始化底部导航栏*/
+        tabHost=(FragmentTabHost)super.findViewById(android.R.id.tabhost);
+        tabHost.setup(this,super.getSupportFragmentManager()
+                ,R.id.contentLayout);
+        tabHost.getTabWidget().setDividerDrawable(null);
+        tabHost.setOnTabChangedListener(this);
+        initTab();
+    }
 
-        //使用FragmentTabHost去添加子标签,根据fragment集合里有多少个fragment,动态的进行添加.
-        for (int x = 0; x < fragments.length; x++) {
-            //把一个布局的XML资源转换为一个View的对象,第一个参数就是layout,第二个设置为空就行
-            View inflate = getLayoutInflater().inflate(R.layout.nav_header, null);//注意,如果这个View对象下有子控件,就用这个控件对象去查找
-            //进行控件查找
-            tab_iv = (ImageView) inflate.findViewById(R.id.icon_image);
-            tab_tv = (TextView) inflate.findViewById(R.id.textView);
-            //为这些子控件设置图片文本资源,从对应的集合里拿
-            tab_iv.setImageResource(imgSelectors[x]);
-            tab_tv.setText(tabTitle[x]);
-            //使用FragentTabHost去添加子标签核心代码,TabHost,newTabSpec(""+x)为每个字标签添加标识,并添加加载显示的底部view
-            //第二参数为添加动态加载的fragment对象,从集合里动态的那fragment,第三个参数,给null即可;
-            tabHost.addTab(tabHost.newTabSpec("" + x).setIndicator(inflate), fragments[x], null);
+    /*导航栏*/
+    private void initTab(){
+        String tabs[]= TabDb.getTabsTxt();
+        for(int i=0;i<tabs.length;i++){
+            TabHost.TabSpec tabSpec=tabHost.newTabSpec(tabs[i]).setIndicator(getTabView(i));
+            tabHost.addTab(tabSpec,TabDb.getFragments()[i],null);
+            tabHost.setTag(i);
+        }
+    }
+    private View getTabView(int idx){
+        View view= LayoutInflater.from(this).inflate(R.layout.fragment_tab,null);
+        ((TextView)view.findViewById(R.id.tvTab)).setText(TabDb.getTabsTxt()[idx]);
+        if(idx==0){
+            ((TextView)view.findViewById(R.id.tvTab)).setTextColor(Color.RED);
+            ((ImageView)view.findViewById(R.id.ivImg)).setImageResource(TabDb.getTabsImgLight()[idx]);
+        }else{
+            ((ImageView)view.findViewById(R.id.ivImg)).setImageResource(TabDb.getTabsImg()[idx]);
+        }
+        return view;
+    }
+
+    @Override
+    public void onTabChanged(String tabId) {
+        // TODO Auto-generated method stub
+        updateTab();
+    }
+    private void updateTab(){
+        TabWidget tabw=tabHost.getTabWidget();
+        for(int i=0;i<tabw.getChildCount();i++){
+            View view=tabw.getChildAt(i);
+            ImageView iv=(ImageView)view.findViewById(R.id.ivImg);
+            if(i==tabHost.getCurrentTab()){
+                ((TextView)view.findViewById(R.id.tvTab)).setTextColor(Color.RED);
+                iv.setImageResource(TabDb.getTabsImgLight()[i]);
+            }else{        ((TextView)view.findViewById(R.id.tvTab)).setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+                iv.setImageResource(TabDb.getTabsImg()[i]);
+            }
+
         }
     }
 
