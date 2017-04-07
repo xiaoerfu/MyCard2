0ea9bb4328d63d6f2e99de477020e9db4fd2f21e
lijianfu <3067418132@qq.com>
2017-04-07 17:49:24 +0800

    修改初始化布局

--- app/src/main/java/com/example/mycard/view/LoginActivity.java
+++ app/src/main/java/com/example/mycard/view/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.mycard.view;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
